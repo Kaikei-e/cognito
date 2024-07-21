d29bc4283ee3f51a214749ba96be60ef20e87c7d
Kaikei-e <e_kaikei@protonmail.com>
2024-07-21 23:39:25 +0900

    decide the bg color for relaxing mood

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import { ExplicateIndex } from "@/app/explicate";
+import {ExplicateIndex} from "@/app/explicate";
 import React from "react";
-import { SchemaIndex } from "@/app/schema";
+import {SchemaIndex} from "@/app/schema";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <ExplicateIndex />
-        <SchemaIndex />
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-16 bg-fuchsia-50">
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                <ExplicateIndex/>
+                <SchemaIndex/>
+            </div>
+        </main>
+    );
 }
