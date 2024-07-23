ad9cb8cc6eaef2e5c78258583b1bf6b1297b10e6
Kaikei-e <e_kaikei@protonmail.com>
2024-07-23 21:03:10 +0900

    chore: Update theme colors and RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
-import {Providers} from './providers';
-import RecoilProvider from './RecoilRootWrapper';
+import { Providers } from "./providers";
+import RecoilProvider from "./RecoilRootWrapper";
 import React from "react";
+import theme from "@/theme";
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-        <body>
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
         <RecoilProvider>
-            <Providers>{children}</Providers>
+          <Providers>{children}</Providers>
         </RecoilProvider>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import {extendTheme} from "@chakra-ui/theme-utils";
+
+const theme = extendTheme(
+    {
+        colors: {
+            faintPink: {
+                50: "#fde7fc"
+            }
+        }
+    }
+)
+
+export default theme;
